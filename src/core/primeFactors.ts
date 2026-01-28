9ec496870a9c2208f671e42a0a5a91bb707a3865
Luis <luis.amez.vitoria@gmail.com>
2026-01-28 19:59:16 +0100

    Implement functionality and tests for getting the prime factors of a given number

--- src/core/primeFactors.ts
+++ src/core/primeFactors.ts
@@ -1,3 +1,24 @@
-export function primeFactors(number: number) {
-  return number;
+export function getPrimeFactorsFor(number: number) {
+  validateInput(number);
+
+  const factors: number[] = [];
+  let currentFactor = 2;
+  let remainder = number;
+
+  while (remainder !== 1) {
+    while (remainder % currentFactor === 0) {
+      factors.push(currentFactor);
+      remainder /= currentFactor;
+    }
+    currentFactor++;
+  }
+
+  return factors;
+}
+
+function validateInput(number: number) {
+  if (number === 1) throw new Error('Invalid input: 1 is not a prime number');
+  if (number === 0) throw new Error('Invalid input: 0');
+  if (number < 0) throw new Error('Invalid input: negative number');
+  if (!Number.isInteger(number)) throw new Error('Invalid input: non-integer');
 }
--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -1,9 +1,39 @@
-import { primeFactors } from '../core/primeFactors';
+import { getPrimeFactorsFor } from '../core/primeFactors';
 
 describe('primeFactors', () => {
-  it('should return 2 for 2', () => {
-    const result = primeFactors(2);
+  it('fails for 1, given that 1 is not a prime number by definition', () => {
+    expect(() => getPrimeFactorsFor(1)).toThrow('Invalid input: 1 is not a prime number');
+  });
 
-    expect(result).toBe(2);
+  it('fails for 0', () => {
+    expect(() => getPrimeFactorsFor(0)).toThrow('Invalid input: 0');
+  });
+
+  it('fails for negative numbers', () => {
+    expect(() => getPrimeFactorsFor(-1)).toThrow('Invalid input: negative number');
+  });
+
+  it('fails for non-integers', () => {
+    expect(() => getPrimeFactorsFor(1.5)).toThrow('Invalid input: non-integer');
+  });
+
+  it('works for prime numbers', () => {
+    expect(getPrimeFactorsFor(2)).toEqual([2]);
+    expect(getPrimeFactorsFor(3)).toEqual([3]);
+    expect(getPrimeFactorsFor(5)).toEqual([5]);
+  });
+
+  it('works for powers of the same prime number', () => {
+    expect(getPrimeFactorsFor(27)).toEqual([3, 3, 3]);
+    expect(getPrimeFactorsFor(49)).toEqual([7, 7]);
+  });
+
+  it('works for numbers with different prime factors', () => {
+    expect(getPrimeFactorsFor(12)).toEqual([2, 2, 3]);
+    expect(getPrimeFactorsFor(105)).toEqual([3, 5, 7]);
+  });
+
+  it('orders the prime factors in ascending order', () => {
+    expect(getPrimeFactorsFor(5 * 7 * 13 * 2 * 2 * 3 * 3)).toEqual([2, 2, 3, 3, 5, 7, 13]);
   });
 });
